194ae5b86872738946f302a28e1b21380a7e6951
hasan-furkan <hsnfrkn32@gmail.com>
2021-08-17 20:42:35 +0300

    portfolio edit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import "./portfolio.scss";
 
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio"> 
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Design</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Tour Places</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Tour Places</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Tour Places</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Tour Places</h3>
+                </div>
+            </div>
         </div>
     )
 }
