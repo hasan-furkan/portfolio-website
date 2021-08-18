c4d1bb6271f41cd14ace81791787582458b3189e
hasan-furkan <hsnfrkn32@gmail.com>
2021-08-18 19:23:24 +0300

    works.edit

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,27 @@ import "./work.scss"
 export default function Works() {
     return (
         <div className="works" id="works">
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/mobile.png" alt="" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit temporibus reprehenderit voluptatum sint, aperiam fuga iure sed.</p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src="https://cdn.pixabay.com/photo/2021/08/08/15/45/mountains-6531264_1280.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img src="assets/arrow.png" className="arrow left" alt="" />
+            <img src="assets/arrow.png" className="arrow right" alt="" />
         </div>
     )
 }
